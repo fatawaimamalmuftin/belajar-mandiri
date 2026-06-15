ff99398a00b9317717f184ca0682d3113cfe7222
fatawaimamalmuftin <muftinmarmut@gmail.com>
2026-06-15 22:15:46 +0700

    docs: learn by my self

--- belajarReact/src/App.jsx
+++ belajarReact/src/App.jsx
@@ -1,78 +1,72 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react"
 
-function App() {
+export default function App() {
+    // const [data, setData] = useState([])
 
-  const [characters, setCharacters] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [searchQuery, setSearchQuery] = useState("");
+    // useEffect(() => {
+    //     const getData = async () => {
+    //         const req = await fetch("https://jsonplaceholder.typicode.com/users")
+    //         setData(await req.json())
+    //     }
+    //     getData();
+    // }, [])
 
-  const filteredCharacters = characters.filter((char) => {
-    return char.name.toLowerCase().includes(searchQuery.toLowerCase());
-  })
+    // console.log(data)
+    {/* {
+        data.map((item) => {
+            return (
+                <div key={item.name} className="flex flex-col justify-center items-center p-4">
+                    <div className="flex justify-center items-center">
+                        {item.name}
+                    </div>
+                    <div className="flex justify-center items-center">
+                        {item.email}
+                    </div>
+                </div>
+            )
+        })
+    } */}
+    // const id = form.get("id")
+    // const pass = form.get("pass")
+    // console.log(id)
+    // console.log(pass)
 
-  useEffect(() => {
-    const fetchCharacters = async () => {
-      try {
-        setIsLoading(true);
-        const res = await fetch("https://rickandmortyapi.com/api/character")
-        if (!res.ok) {
-          throw new Error(`Gagal ambil data, Status Error ${res.status}`);
-        }
-        const data = await res.json();
-        setCharacters(data.results);
-        // console.log(data.results);
-      } catch (Error) {
-        console.error(`Error: ${Error}`)
-      } finally {
-        setIsLoading(false);
-      }
+
+    function hendleForm(event) {
+        event.preventDefault()
+
+        const form = new FormData(event.target)
+
+        const data = Object.fromEntries(form.entries())
+
+        const local = JSON.parse(localStorage.getItem("tes") || "[]")
+
+
+        local.push(data)
+
+        localStorage.setItem("tes", JSON.stringify(local))
     }
 
-    fetchCharacters();
-  }, []);
 
+    return (
 
-  return (
-    <main className="p-8 bg-gray-900 min-h-screen text-white">
-      <h1 className="text-3xl font-bold mb-6 text-center">
-        Rick & Morty Characters
-        {/* kolom pencarian dan daftar karakter bakal di sini */}
-      </h1>
-      <div className="flex justify-center mb-8">
-        <input type="text"
-          placeholder="Cari character"
-          className="px-4 py-2 w-full max-w-md rounded bt-gray-800 border border-gray-700 text-white focus:outline-none focus:border-blue-500"
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)} />
-      </div>
-      {isLoading ? (
-        <div className="text-center text-xl font-semibold animate-pulse text-blue-400">
-          Sedang memuat data .......
-        </div>
-      ) : (
-        filteredCharacters.length === 0 ? (
-          <div>
-            character "{searchQuery}" tidak di temukan
-          </div>
-        ) : (
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-            {filteredCharacters.map((char) => (
-              <div key={char.id} className="bg-gray-800 rounded-lg overflow-hiden shadow-lg border border-gray-700 hover:border-blue-500 transition">
-                <img src={char.image} alt={char.name} className="w-full h-48 object-cover" />
-                <div className="p-4">
-                  <h2 className="text-xl font-bold truncate">{char.name}</h2>
-                  <p className="text-gray-400 text-sm mt-1">Species: {char.species}</p>
-                  <span className={`inline-block px-2 py-1 text-xs font-semibold rounded mt-2 ${char.status === "Alive" ? "bg-green-600" : char.status === "Dead" ? "bg-red-600" : "bg-gray-600"}`}>{char.status}</span>
+        <>
+            <main>
+                <div className="flex justify-center items-center p-20">
+                    <form onSubmit={hendleForm} className="flex flex-col border-2 border-blue-500 gap-2">
+                        <div className="flex gap-2 border-2 border-purple-500">
+                            <label htmlFor="id">Id :</label>
+                            <input type="text" name="id" />
+                        </div>
+
+                        <div className="flex gap-2 border-2 border-purple-500">
+                            <label htmlFor="pass">pass :</label>
+                            <input type="password" name="pass" />
+                        </div>
+                        <button type="submit" className="flex items-center justify-center gap-2 border-2 border-purple-500">Submit</button>
+                    </form>
                 </div>
-              </div>
-            ))}
-          </div>
-        )
-      )};
-    </main>
-
-  )
-
+            </main >
+        </>
+    )
 }
-
-export default App;
